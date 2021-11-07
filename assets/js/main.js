871d104d658d28056d51414e43bac64949f8d112
Valen-chaveli <valen7est@gmail.com>
2021-11-07 16:53:22 +0100

    Profesional education added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,82 @@
 import { data } from "../../config/data.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let arrIdItems = getIdForExperiences();
-    setDataForExperience(arrIdItems);
+    addExperineces();
+    addEducation();
 })
 
+function addExperineces() {
 
-function setDataForExperience(arrExperiencesIdFromDOM) {
+    let { experiences } = data
 
-    let { experiences } = data;
+    let experiencesContainer = document.querySelector("#cv-professional-experience");
 
-    experiences.forEach(({ id , period, company, job, tasks}) => {
-        arrExperiencesIdFromDOM.forEach((idFromDOM) =>{
-            if(idFromDOM == id) {
-               let element = document.getElementById(id);
+    experiences.forEach(({ period, duration, company, job, tasks }) => {
+        
+        let experienceContainer = document.createElement("div");
+        experienceContainer.classList.add("section-content", "section-content-grid");
+        
+        // Period
+        let periodParentContainer = document.createElement("div");
+        periodParentContainer.classList.add("period-box");
 
-               element.querySelector(".period-box").innerHTML = period
-               element.querySelector(".job-box").innerHTML = `<p>${job}<p>`
+        let durationContainer = document.createElement("div");
+        durationContainer.innerHTML = duration;
 
-               tasks.forEach((task) => {
-                element.querySelector(".some-task-box ul").innerHTML += `<li>${task}</li>
-                `
-               })
+        let periodContainer = document.createElement("div");
+        periodContainer.innerHTML = period;
 
-              
-               console.log(element);
-            }
-        }) 
-    })
+        periodParentContainer.append(durationContainer, periodContainer);
+        
+        //Job
+        let jobParentContainer = document.createElement("div");
+        jobParentContainer.classList.add("job-box");
+        jobParentContainer.innerHTML = job + " on " + company;         
+
+        //Tasks
+        let someTasksParentContainer = document.createElement("div");
+        someTasksParentContainer.classList.add("some-task-box");
+
+        tasks.forEach((task) => {
+            let taskItem  = document.createElement("div");
+            taskItem.innerText = task;
+            someTasksParentContainer.append(taskItem);
+        });
+
+        experienceContainer.append(periodParentContainer, jobParentContainer, someTasksParentContainer);
+        experiencesContainer.append(experienceContainer);     
+    });
 }
 
+function addEducation() {
 
-function getIdForExperiences() {
-    let arrExperienceItems = document.querySelectorAll("#cv-profesional-experience .section-content-grid");
-    
-    let arrIds = [];
+    let { education } = data;
 
-    arrExperienceItems.forEach((experienceItem) => {
-        arrIds.push(experienceItem.id)
+    let educationParentContainer = document.querySelector("#cv-professional-education");
+
+    education.forEach(({period, titleName, schoolName}) => {
+
+        let educationContainer = document.createElement("div");
+        educationContainer.classList.add("section-content", "section-content-grid");
+
+        let periodParentContainer = document.createElement("div");
+        periodParentContainer.classList.add("period-box");
+        periodParentContainer.innerHTML = period;
+
+        let titleParent = document.createElement("div");
+        titleParent.classList.add("job-box");
+
+        let educationTitleNameContainer = document.createElement("div");
+        educationTitleNameContainer.innerHTML = titleName;
+
+        let educationSchooldNameContainer = document.createElement("div");
+        educationSchooldNameContainer.innerHTML = schoolName;
+
+        titleParent.append(educationTitleNameContainer, educationSchooldNameContainer)
+
+        educationContainer.append(periodParentContainer, titleParent);
+
+        educationParentContainer.append(educationContainer);
     });
 
-    return arrIds;
 }
--- config/data.js
+++ config/data.js
@@ -3,35 +3,41 @@ export const data = {
     experiences : [
         {
             id : "experience-1",
-            period : "10-03-21",
+            duration: "9 months",
+            period : "10-03-21 - 10-03-21",
             company : "SKLUM S.L",
             job : "Web Developer",
             tasks : [
-                "some_task","some_task","some_task"
+                "- Develop, implement and maintain new features with PHP in Prestashop",
+                "- Microservices development with Nodejs using Hexagonal Architecture, SOLID and DDD design patterns",
+                "- End to end test development with Cypress",
+                "- Migrations and query operations with Mysql, PostgreSql and MariaDB"
             ]
         },
         {
             
             id : "experience-2",
-            period : "10-03-22",
-            company: "",
-            job : "",
+            duration : "3 months",
+            period : "25-03-19 - 04-05-19",
+            company: "DULCESOL S.L",
+            job : "Computer Technician",
             tasks : [
-                "","",""
+                "- some tasks",
+                "- some tasks"
             ]
         }
     ],
 
     education : [
         {
-            period : "",
-            titleName : "",
-            schoolName : ""
+            period : "2019-2021",
+            titleName : "Certificate of Higher Education of Web Application Development",
+            schoolName : "IES MARÍA ENRÍQUEZ"
         },
         {
-            period : "",
-            titleName : "",
-            schoolName : ""
+            period : "2017-2019",
+            titleName : "Vocational Education and Training of Systems Microcomputers and networks",
+            schoolName : "IES MARÍA ENRÍQUEZ"
         }
     ]
 }
